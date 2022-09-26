2f4a1fdd3e96e95bcf0bf87936fc8c78669c9acc
jsantiago99 <70917606+jsantiago99@users.noreply.github.com>
2022-09-26 09:54:40 -0600

    Update streamlit_app.py
    Display only fruits selected

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,10 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit') #this is to set Fruit name instead of index
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries']) #addes the multiselect, last parameter prepopulates the field
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries']) #addes the multiselect, last parameter prepopulates the field
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-streamlit.dataframe(my_fruit_list) #shows the list of fruits
+streamlit.dataframe(fruits_to_show) #shows the list of fruits
+
+
 
