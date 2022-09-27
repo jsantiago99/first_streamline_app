7a16878ee8125d21645a991d1e80c1df46f2bb6d
jsantiago99 <70917606+jsantiago99@users.noreply.github.com>
2022-09-27 11:44:00 -0600

    Update streamlit_app.py
    cleaned up add my fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,10 @@ def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("select * from fruit_load_list")
         return my_cur.fetchall()
-   
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        return 'Thanks for adding' + new_fruit
     
     
 
@@ -56,8 +59,11 @@ if streamlit.button('Get Fruit Load List'):
 
 
 
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
 
-fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding', fruit_choice)
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
 
